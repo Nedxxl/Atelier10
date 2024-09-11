11d15515d8d5597adab681f698e955c193c138d8
Nedxxl <emryc.perichaud@hotmail.fr>
2024-09-11 13:53:30 -0400

    V1.1

--- atelier6.js
+++ atelier6.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res, next) {
-    res.send('<h1>Bienvenue dans mon Super Site</h1>');
+    res.render('pages/index');
 });
 
 app.get('/contact', function(req, res, next) {
