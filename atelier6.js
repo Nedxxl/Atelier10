1b6634b956ac6f4a0257ce74480003b5a6b8702e
Nedxxl <emryc.perichaud@hotmail.fr>
2024-09-23 12:08:18 -0400

    V3.final

--- atelier6.js
+++ atelier6.js
@@ -10,31 +10,38 @@ var minute = now.getMinutes();
 var seconde = now.getSeconds();
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res, next) {
-    res.render('pages/index',{ annee: annee, mois: mois, jour: jour, heure: heure, minute: minute, seconde: seconde });
+app.get('/', function (req, res, next) {
+    res.render('pages/index', { annee: annee, mois: mois, jour: jour, heure: heure, minute: minute, seconde: seconde });
 });
 
-app.get('/contact', function(req, res, next) {
+app.get('/contact', function (req, res, next) {
     res.send('<h2> Vous êtes dans la page de contact </h2> <p> Voici nos coordonnées notascam@ruppee.in </p> <a href="/"> Retour à l\'accueil </a>');
 });
 
-app.get('/recherche', function(req, res) {
+app.get('/recherche', function (req, res) {
     var mois = req.query.mois;
     var annee = req.query.annee;
     res.send('Silly willy et le weiny magique en: ' + mois + '/' + annee);
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.status(404).send('Error 404: Page not found');
 });
 
 var server = app.listen(8080);
-var io = require('socket.io') (server);
+var io = require('socket.io')(server);
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
     cntClient++;
-    
-app.set('view engine', 'ejs');
+
+    app.set('view engine', 'ejs');
     console.log('Un client est connecté');
     socket.emit('message', 'Vous êtes le ' + cntClient + 'ème client');
+    socket.on('message', function (message) {
+        console.log('Un client me parle ! Il me dit : ' + message);
+        socket.emit('hours', message);
+        socket.broadcast.emit('hours', message);
     });
+
+    
+});
